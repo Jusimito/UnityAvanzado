9a8e9040da9f4cb83100f823ec8dfdb42646fd1d
Diego Sanchez <diesarar@gmail.com>
2024-11-22 17:37:55 +0100

    + primera implementacion de movimiento con Input System

--- UnityAvanzado/Assets/Scripts/PlayerMovement.cs
+++ UnityAvanzado/Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,73 @@ using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float movementSpeed = 15.0f;
+    [SerializeField] private float maxMovementSpeed = 15.0f;
     [SerializeField] private float rotationSpeed = 50.0f;
+    [SerializeField] private float acelerationTime = 1f;
+    [SerializeField] private AnimationCurve acelerationCurve;
+    [SerializeField] private float decelerationTime = 1f;
+    [SerializeField] private AnimationCurve decelerationCurve;
 
-    float verticalInputAmount;
-    float horizontalInputAmount;
+    Vector2 lastInputValue, inputValue = Vector2.zero;
+    float currentSpeed = 0f;
+    bool acelerating, decelerating = false;
 
     public void OnPlayerMove(InputAction.CallbackContext callbackContext)
     {
-        verticalInputAmount = callbackContext.ReadValue<Vector2>().y;
-        horizontalInputAmount = callbackContext.ReadValue<Vector2>().x;
+        lastInputValue = inputValue;
+        inputValue = callbackContext.ReadValue<Vector2>();
+
+        if(lastInputValue == Vector2.zero && inputValue != lastInputValue && !acelerating)
+        {
+            acelerating = true;
+            decelerating = false;
+            StartCoroutine(Acelerate());
+        }
+
+        if(inputValue == Vector2.zero && inputValue != lastInputValue && !decelerating)
+        {
+            acelerating = false;
+            decelerating = true;
+            StartCoroutine(Decelerate());
+        }
+
+        if(!acelerating && !decelerating)
+        {
+            currentSpeed = maxMovementSpeed;
+        }
+    }
+
+    private IEnumerator Acelerate()
+    {
+        float time = 0;
+        while (time <= acelerationTime)
+        {
+            Debug.Log("Acelerating: " + currentSpeed);
+            currentSpeed = maxMovementSpeed * acelerationCurve.Evaluate(time / acelerationTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        acelerating = false;
+    }
+
+    private IEnumerator Decelerate()
+    {
+        float time = 0;
+        while (time <= decelerationTime)
+        {
+            Debug.Log("Decelerating: " + currentSpeed);
+            currentSpeed = maxMovementSpeed * decelerationCurve.Evaluate(time / decelerationTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        decelerating = false;
     }
 
     private void Update()
     {
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + Vector3.up * rotationSpeed * horizontalInputAmount * Time.deltaTime);
-        transform.Translate(transform.forward * movementSpeed * verticalInputAmount * Time.deltaTime, Space.World);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + Vector3.up * rotationSpeed * inputValue.x * Time.deltaTime);
+        transform.Translate(transform.forward * currentSpeed * inputValue.y * Time.deltaTime, Space.World);
     }
 }
